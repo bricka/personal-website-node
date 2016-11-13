34daca617eb51afe32adf0aac39b432835e8ba23
Alex Brick <alexbrick@porch.com>
2016-11-12 17:24:22 -0800

    More fixes!

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './styles/app.scss';
 
 export default class App extends Component {
   render = () => (
-    <div className="App">
-      <div className="App-header">
+    <div className="app">
+      <div className="app-header">
         <h2>Welcome to React</h2>
       </div>
 
-      <p className="App-intro">
+      <p className="app-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
     </div>
