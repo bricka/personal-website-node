f494b59e6ed3659de0f891dbcd83df8b5c82277f
Alex Brick <alexbrick@porch.com>
2016-11-12 17:33:18 -0800

    Using React Router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+
+const router = (
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="resume" component={Resume}/>
+            <Route path="courses" component={Courses}/>
+            <Route path="projects" component={Projects}/>
+            <Route path="contact" component={Contact}/>
+        </Route>
+    </Router>
+);
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  router,
+  document.body
 );
