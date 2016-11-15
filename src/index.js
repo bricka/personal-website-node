ecc90f8e041a4d2e0bb97e3699f637630d837456
Alex Brick <alexbrick@porch.com>
2016-11-14 17:44:04 -0800

    Adding Home component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+
+import Home from './home';
 
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
