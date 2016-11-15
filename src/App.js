67aadeab600aa13b9f556a0e2e215aa51ff55490
Alex Brick <alexbrick@porch.com>
2016-11-14 19:08:11 -0800

    Dependency on recompose

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import './styles/app.scss';
 
 import Header from './header';
 
+import { withState } from 'recompose';
+
 export default class App extends Component {
+  static propTypes = {
+    currentLanguage: PropTypes.string.isRequired,
+    onLanguageChange: PropTypes.func.isRequired
+  };
+
   render = () => (
     <div className="app">
-      <Header/>
+      <Header
+        currentLanguage={this.props.currentLanguage}
+        onLanguageChange={this.props.onLanguageChange}
+      />
 
       <div className="body">
         {this.props.children}
