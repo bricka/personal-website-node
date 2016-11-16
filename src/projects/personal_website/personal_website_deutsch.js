4464e6f3d014ef97ea289a6c3791b3c500048957
Alex Brick <alexbrick@porch.com>
2016-11-16 14:49:38 -0800

    Adding personal website content

--- src/projects/personal_website/personal_website_deutsch.js
+++ src/projects/personal_website/personal_website_deutsch.js
@@ -3,6 +3,23 @@ import React from 'react';
 export default function PersonalWebsiteProjectDeutsch() {
   return (
     <div>
+      <p>
+        Diese Seite ist eines meiner sichtbareren Projekten.  Diese Siete wird mit Node.js und React geschreiben, und wird mit Heroku eingesetzt.
+      </p>
+
+      <p>
+        Ich habe in meiner Karriere viele dieser Technologien benutzt, aber ich habe die selten von Grund benutzt.  Ich habe auch nie React von selbst benutzt.  Ich habe gewöhnlich mit einem Flux-System benutzt.
+      </p>
+
+      <p>
+        Meine alte persönliche Seite wurde in PHP geschreiben, so dies ist viel besser und moderner.  Sie können das Quellcode hier finden:
+      </p>
+
+      <p>
+        <a href="https://github.com/bricka/personal-website-node">
+          Quellcode von Seite
+        </a>
+      </p>
     </div>
   );
 }
