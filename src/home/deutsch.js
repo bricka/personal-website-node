27e3814e0c33f3c9f8e2d92a41cd2956a805e85d
Alex Brick <alex@alexbrick.me>
2019-10-14 10:06:05 +0200

    Updating the home page with new work status and German translation

--- src/home/deutsch.js
+++ src/home/deutsch.js
@@ -1,15 +1,47 @@
 import React from 'react';
 
-import HomeEnglish from './english';
-
 export default function HomeDeutsch() {
   return (
     <div>
-      <p className="comment">
-        Ich lerne Deutsch, aber ich bin nicht perfekt.  Diese Seite ist auf Englisch, bis ich es neu schreibe, aber die weitere Seite sind doch auf Deutsch.
+      <p>
+        Mein Name ist Alex Brick, und ich bin Software Entwickler (und manchmal Manager) in München. Ich genieße es, wenn ich Werkzeuge und Software entwickeln kann, die Benutzer-Anforderungen wirklich lösen.
       </p>
 
-      <HomeEnglish/>
+      <p>
+        Meine Interesse in Komputern fing vor vielen vielen Jahren an, als ich nur 10-Jähriger war. Ich erinnere mich noch an mein erstes "Nummer-Rat" Spiel. Obwohl das Programm keine Schleifen, keine Ifs, keine nichts hatte. Vielleicht war das allerdings nicht das allerbeste Programm...
+      </p>
+
+      <p>
+        Auf jedem Fall hat alles sich geändert. Nachdem ich im Gymnasium AP Programming nahm und Object-Oriented Programming entdeckte, fing ich an zu lernen, wie breit und interessant Computer Science wirklich ist. Ich machte ein Praktikum bei den National Institutes of Health, wo ich Software entwickelte, das wirklich verwendet wurde. In 2005 veröffentlichte ich meine erste Applikation, lineaksetup, ein Konfiguration-Werkzeug für das <a href="http://lineak.sourceforge.net">LinEAK</a> Projekt.
+      </p>
+
+      <p>
+        In 2006 studierte ich Computer Science bei <a href="http://northeastern.edu">Northeastern University</a> in Boston. Dort arbeitete ich mit der High Performance Computing Group, wo ich an "low-level user-level process checkpointing" arbeitete.
+      </p>
+
+      <p>
+        In der zweiten Hälfte von 2008 machte ich ein 6-Monaten-lang Praktikum bei Amazon.com, wo ich Mitglieder im Client Experience Analytics Team war. Die Erfahrung war wunderbar, wo ich mit viel Autonomie arbeiten durfte und viele interessante Aufgaben hatte.
+      </p>
+
+      <p>
+        In der zweiten Hälfte von 2009 studierte ich in Tokio, Japan bei <a href="http://www.obirin.ac.jp/en">J.F. Oberlin University</a>. Dort studierte ich die japanische Sprache und Kultur, mit dem möglichen Ziel irgendwann im Zukunft in Japan zu arbeiten oder wohnen. Ich fand und finde noch Japan ein sehr interessantes Land.
+      </p>
+
+      <p>
+        In der zweiten Hälfte von 2010 kam ich zu Amazon.com zurück, wo ich in einem anderen Team arbeitete, das Software Deploment Team. Hier müsste ich ein ganz neues System designen und entwickeln. Ich lernte viel über gutes Design und durfte wieder mit viel Autonomie arbeiten.
+      </p>
+
+      <p>
+        Nach meinem Abschluss kam ich zu Amazon.com wieder und wieder mit dem Software Deployment Team arbeitete. Nach ein paar Jahren wurde ich das Team Lead. Nach Amazon kam ich zu Porch, eine Firma, die Software entwickelt, um Hausbezitzer mit Hausarbeit-Profis zu verbinden. Bei Porch arbeitete ich viel auf dem Frontend und lernte modernere Frameworks und viel über gute User Experience.
+      </p>
+      
+      <p>
+        In 2017 entschied ich nach München umzuziehen, wo ich bei Definiens AG anfing zu arbeiten, eine Firma, die Software entwickelt um Image Analysis für Krebsbilder zu schaffen. Bei Definiens arbeitete ich viel mit User-Anforderungen und modinisierte viel von der Software-Entwicklung-Infrastruktur. Ich arbeitete auch für sieben Monate als Manager, und das war eine interessante Erfahrung.
+      </p>
+
+      <p>
+        Ich liebe immer noch, neue Technologien zu lernen, und ich freue mich auf viele neue und interessant Probleme!
+      </p>
     </div>
   );
 }
