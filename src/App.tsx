56ece667554662bdfaf0a909ee363a16ab20b935
Alex Brick <alex@alexbrick.me>
2020-04-07 21:28:35 +0200

    Moving BrowserRouter to the top-level of the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './styles/app.scss';
 
 import { IntlProvider } from 'react-intl';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import * as cookies from 'js-cookie';
 
@@ -22,52 +22,50 @@ export default function App() {
   const [currentLanguage, setCurrentLanguage] = React.useState(cookies.get('currentLanguage') || 'en');
 
   const onLanguageChange = (language: string) => {
-      cookies.set('currentLanguage', language);
-      setCurrentLanguage(language);
+    cookies.set('currentLanguage', language);
+    setCurrentLanguage(language);
   };
 
   return (
-    <BrowserRouter>
-      <IntlProvider
-        locale={currentLanguage}
-        messages={messages[currentLanguage]}
-      >
-        <div className="app">
-          <Header
-            currentLanguage={currentLanguage}
-            onLanguageChange={onLanguageChange}
-          />
+    <IntlProvider
+      locale={currentLanguage}
+      messages={messages[currentLanguage]}
+    >
+      <div className="app">
+        <Header
+          currentLanguage={currentLanguage}
+          onLanguageChange={onLanguageChange}
+        />
 
-          <div className="body">
-            <Switch>
-              <Route path="/resume">
-                <Resume currentLanguage={currentLanguage}/>
-              </Route>
-              <Route path="/courses">
-                <Courses/>
-              </Route>
-              <Route path="/projects/mips-vim">
-                <MipsVimHighlightingProject currentLanguage={currentLanguage}/>
-              </Route>
-              <Route path="/projects/giles">
-                <GilesProject currentLanguage={currentLanguage}/>
-              </Route>
-              <Route path="/projects/personal-website">
-                <PersonalWebsiteProject currentLanguage={currentLanguage}/>
-              </Route>
-              <Route path="/projects">
-                <Projects/>
-              </Route>
-              <Route path="/contact">
-                <Contact/>
-              </Route>
-              <Route path="/">
-                <Home currentLanguage={currentLanguage}/>
-              </Route>
-            </Switch>
-          </div>
+        <div className="body">
+          <Switch>
+            <Route path="/resume">
+              <Resume currentLanguage={currentLanguage}/>
+            </Route>
+            <Route path="/courses">
+              <Courses/>
+            </Route>
+            <Route path="/projects/mips-vim">
+              <MipsVimHighlightingProject currentLanguage={currentLanguage}/>
+            </Route>
+            <Route path="/projects/giles">
+              <GilesProject currentLanguage={currentLanguage}/>
+            </Route>
+            <Route path="/projects/personal-website">
+              <PersonalWebsiteProject currentLanguage={currentLanguage}/>
+            </Route>
+            <Route path="/projects">
+              <Projects/>
+            </Route>
+            <Route path="/contact">
+              <Contact/>
+            </Route>
+            <Route path="/">
+              <Home currentLanguage={currentLanguage}/>
+            </Route>
+          </Switch>
         </div>
-      </IntlProvider>
-    </BrowserRouter>
+      </div>
+    </IntlProvider>
   );
 }
