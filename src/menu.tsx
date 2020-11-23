b594bc87da7928d0e78640a2f5e9c3c524046f8a
Alex Brick <alex@alexbrick.me>
2020-11-23 18:49:05 +0100

    Menu

--- src/menu.tsx
+++ src/menu.tsx
@@ -2,12 +2,36 @@ import * as React from 'react';
 
 import { FormattedMessage } from 'react-intl';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const ACTIVE_CLASS_NAME = 'selectedMenuItem';
 
+const MenuWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 5px;
+
+  a {
+  border-width: 1px;
+  border-style: solid;
+  border-color: #D9D9D9 #909090 #909090 #D9D9D9;
+  text-align: center;
+  text-decoration: none;
+  margin: 0px 5px 0px 10px;
+  padding: 2px;
+
+  &:hover, &.selectedMenuItem {
+  background : #B7B7B7;
+  border-color : #909090 #D9D9D9 #D9D9D9 #909090;
+  }
+  }
+`;
+
 export default function Menu() {
   return (
-    <div className="menu">
+    <MenuWrapper>
       <NavLink
         activeClassName={ACTIVE_CLASS_NAME}
         exact
@@ -54,6 +78,6 @@ export default function Menu() {
           defaultMessage="Contact"
         />
       </NavLink>
-    </div>
+    </MenuWrapper>
   );
 }
