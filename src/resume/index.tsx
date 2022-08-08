6cf31b5f403b94d7087f6884268a0a3070b2f7e1
Alex Figl-Brick <alex@alexbrick.me>
2022-08-08 21:09:02 +0200

    Updating resume
    Updating to a newer version of the resume, as well as removing the
    German-language version entirely

--- src/resume/index.tsx
+++ src/resume/index.tsx
@@ -2,55 +2,26 @@ import * as React from 'react';
 
 import { FormattedMessage } from 'react-intl';
 
-import germanResumePath from './resume-de-en.pdf';
-import americanResumePath from './resume.pdf';
+import resumePath from './resume.pdf';
 
-const americanLink = (
-  <p key="resume-us">
-    <a href={americanResumePath}>
-      <FormattedMessage
-        id="resume.my-resume.en"
-        defaultMessage="American-style, English"
-      />
-    </a>
-  </p>
-);
-
-const germanEnLink = (
-  <p key="resume-de">
-    <a href={germanResumePath}>
-      <FormattedMessage
-        id="resume.my-resume.de-en"
-        defaultMessage="German-style, English"
-      />
-    </a>
-  </p>
-);
-
-interface Props {
-  currentLanguage: string;
-}
-
-export default function Resume(props: Props) {
-  function links() {
-    switch(props.currentLanguage) {
-      case 'de':
-        return [ germanEnLink, americanLink ];
-      default:
-        return [ americanLink, germanEnLink ];
-    }
-  }
-
+export default function Resume() {
   return (
     <div>
       <p>
         <FormattedMessage
           id="resume.my-resume-is-available"
-          defaultMessage="My resume is available through these links:"
+          defaultMessage="You can find my resume here:"
         />
       </p>
 
-      {links()}
+      <p key="resume">
+        <a href={resumePath}>
+          <FormattedMessage
+            id="resume.my-resume"
+            defaultMessage="My Resume"
+          />
+        </a>
+      </p>
 
       <p>
         <FormattedMessage
