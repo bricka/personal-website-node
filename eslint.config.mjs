bbf8135c4fb0d518a7c278d4dfeee78967af927f
Alex Figl-Brick <alex@alexbrick.me>
2026-02-24 16:22:28 +0100

    A bunch of `npm audit` and eslint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,11 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+export default defineConfig([
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
+  ...nextVitals,
   eslintPluginPrettierRecommended,
-];
-
-export default eslintConfig;
+]);
